9ed0dfdda7aadcc05948acb9634b0b48a25211e8
Kevin Booth <kdbooth97@gmail.com>
2019-03-26 16:42:27 -0400

    Create User model in redline/models.py and make migration file

--- redline/models.py
+++ redline/models.py
@@ -1,3 +1,25 @@
+"""
+Module that defines the models inside the redline app
+"""
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    """
+    Defines the User database object attributes
+    Fields: user_name, password, first_name, last_name
+    """
+    # user name
+    user_name = models.CharField(max_length=50, null=False, unique=True)
+    #password
+    password = models.CharField(max_length=100, null=False)
+    # first name
+    first_name = models.CharField(max_length=255, null=False)
+    # last name
+    last_name = models.CharField(max_length=255, null=False)
+
+    def __str__(self):
+        return "username: {}, {} {}".format(self.user_name,
+                                            self.first_name,
+                                            self.last_name
+                                           )
