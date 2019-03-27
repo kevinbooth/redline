a6fc3169857a27b77a1b3282e967d87e1f0847e3
Kevin Booth <kdbooth97@gmail.com>
2019-03-27 12:11:56 -0400

    Add authenticate/ endpoint to redline/urls.py to receive auth token

--- redline/urls.py
+++ redline/urls.py
@@ -1,9 +1,11 @@
 """
 Module to define urlpatterns
 """
-from django.urls import path
+from django.urls import path, include
+from rest_framework.authtoken import views
 
 
 urlpatterns = [
-
+    path('register/', include('account.urls')),
+    path('authorize/', views.obtain_auth_token, name= "authorize")
 ]
