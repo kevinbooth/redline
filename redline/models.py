bc19680da8d82a79dc67c5a55ac7054641d9bb47
EZombek <emo92@wildcats.unh.edu>
2019-03-31 13:46:58 -0400

    created the car model

--- redline/models.py
+++ redline/models.py
@@ -1,3 +1,15 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Car(models.Model):
+		user_id = models.ForeignKey(User, unique=True)
+		vin = models.CharField(max_length=17, null=False)
+		year = models.IntegerField(max_length=4, null=False)
+		make = models.CharField(max_length=255, null=False)
+		model = models.CharField(max_length=255, null=False)
+		color = models.CharField(max_length=255, null=False)
+
+		def __str__(self):
+			return "{}, {}".format(self.make, self.model)
+
