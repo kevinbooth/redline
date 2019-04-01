c119c7541c18b3d6ab3f2d38c93960c2506a953c
atoscano199 <ajt1042@wildcats.unh.edu>
2019-03-31 20:57:29 -0400

    Commit before rebase of sprint1

--- config/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import User
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ("user_name", "first_name", "last_name")
