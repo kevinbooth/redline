b8b9cb732989d2ae537284a70781e711534bde99
atoscano199 <ajt1042@wildcats.unh.edu>
2019-03-31 23:19:25 -0400

    Created class BaseViewTest.

--- redline/tests.py
+++ redline/tests.py
@@ -1,24 +1,23 @@
-
+""" Used to test each function of the Redline API"""
 import pdb
 
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework.test import APITestCase, APIClient
 from rest_framework.views import status
-from .models import User
-from .serializers import UserSerializer
+from .models import User, Car
+from .serializers import UserSerializer. CarSerializer
 
-class UserCreationTest(APITestCase):
+
+class BaseViewTest(APITestCase):
     client = APIClient()
 
     @staticmethod
-    def create_user(first_name='',
-                     last_name=''
-                     ):
-        if first_name != '' and last_name != '':
-            User.objects.create(first_name=first_name, last_name=last_name)
+    def create_user(user_name='',first_name='',last_name=''):
+        if first_name != '' and last_name != '' and user_name != '':
+            Pilot.objects.create(user_name=user_name, first_name=first_name, last_name=last_name)
 
     def setUp(self):
-        self.create_user('Anthony', 'Toscano')
-        self.create_user('Kevin', 'Booth')
-        self.create_user('Ethan', 'Jarzombek')
+        self.create_user('atoscano199', 'Anthony', 'Toscano')
+        self.create_user('kevinbooth', 'Kevin', 'Booth')
+        self.create_user('EZombek', 'Ethan', 'Jarzombek')
