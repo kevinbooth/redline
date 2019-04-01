eb1d30138a4c2353cfc969a42498a6a9efde7571
atoscano199 <ajt1042@wildcats.unh.edu>
2019-03-31 22:21:48 -0400

    Edited the serializers module and created the user class for the models module.

--- redline/serializers.py
+++ redline/serializers.py
@@ -1,12 +1,15 @@
+"""Used to convert data to Python data types then rendered to JSON"""
 from rest_framework import serializers
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
+        """Maps serializer's files to model fields"""
         model = User
         fields = ("user_name", "first_name", "last_name")
 
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
+        """Maps serializer's files to model fields"""
         model = Car
         fields = ("vin", "year", "make", "model", "color")
