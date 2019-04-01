8551ca83978e007a2d8f6d99b10be98bb43bb206
atoscano199 <ajt1042@wildcats.unh.edu>
2019-04-01 00:46:13 -0400

    Created CarEndpointTest class.

--- redline/tests.py
+++ redline/tests.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 from rest_framework.test import APITestCase, APIClient
 from rest_framework.views import status
 from .models import User, Car
-from .serializers import UserSerializer. CarSerializer
+from .serializers import UserSerializer, CarSerializer
 
 
 class BaseViewTest(APITestCase):
@@ -16,23 +16,25 @@ class BaseViewTest(APITestCase):
     def create_user(user_name='',first_name='',last_name=''):
         if first_name != '' and last_name != '' and user_name != '':
             User.objects.create(user_name=user_name, first_name=first_name, last_name=last_name)
-
-    def setUp(self):
-        self.create_user('atoscano199', 'Anthony', 'Toscano')
-        self.create_user('kevinbooth', 'Kevin', 'Booth')
-        self.create_user('EZombek', 'Ethan', 'Jarzombek')
-        self.create_car('JTMYFREVXD5002907', '2006', 'Subaru', 'Outback', 'Black')
-        self.create_car('3GNGC26U45G236804', '1995', 'GMC', 'Sierra', 'Green')
-        self.create_car('1GNDV13E03D183789', '2018', 'Tesla', 'Model S', 'White')
-
+    @staticmethod
     def create_car(vin='',year='',make='', model='', color=''):
             if vin != '' and year != '' and make != '' and model != '' and color != '':
                 Car.objects.create(vin=vin, year=year, make=make, model=model, color=color)
 
+    def setUpUser(self):
+        self.create_user('atoscano199', 'Anthony', 'Toscano')
+        self.create_user('kevinbooth', 'Kevin', 'Booth')
+        self.create_user('EZombek', 'Ethan', 'Jarzombek')
+
+    def setUpCar(self):
+        self.create_car('JTMYFREVXD5002907', '2006', 'Subaru', 'Outback', 'Black')
+        self.create_car('3GNGC26U45G236804', '1995', 'GMC', 'Sierra', 'Green')
+        self.create_car('1GNDV13E03D183789', '2018', 'Tesla', 'Model S', 'White')
+
 class UserEndpointTest(BaseViewTest):
     def test_get_action(self):
         """
-        This test ensure that all users added in the setUp method
+        This test ensure that all users added in the setUpUser method
         exist when we make a GET request to the user/ endpoint
         """
     response = self.client.get(
@@ -51,6 +53,33 @@ class UserEndpointTest(BaseViewTest):
     post_data = {'user_name': 'atoscano199', 'first_name': 'Anthony', 'last_name': 'Toscano'}
     response = self.client.post(
             reverse("user", kwargs={'version': 'v1'}),
+            post_data,
+            format='json'
+        )
+    self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+class CarEndpointTest(BaseViewTest):
+    def test_get_action(self):
+        """
+        This test ensure that all cars added in the setUpCar method
+        exist when we make a GET request to the car/ endpoint
+        """
+    response = self.client.get(
+        reverse("car", kwargs={"version": "v1"})
+    )
+    expected = Car.objects.all()
+    serialized = CarSerializer(expected, many=True)
+    self.assertEqual(response.data, serialized.data)
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_post_action(self):
+        """
+        This test ensures that a new car can be created via a POST request
+        to the car/ endpoint
+        """
+    post_data = {'vin': 'JTMYFREVXD5002907', 'year': '2006', 'make': 'Subaru', 'model': 'Outback', 'color': 'Black'}
+    response = self.client.post(
+            reverse("car", kwargs={'version': 'v1'}),
             post_data,
             format='json'
         )
