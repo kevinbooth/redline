73d342d6e606ee3e9b616d7428e497bab4b6fc6b
atoscano199 <ajt1042@wildcats.unh.edu>
2019-03-31 21:26:23 -0400

    Edited the serializers module and added car serializer.

--- redline/serializers.py
+++ redline/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from .models import User
 
-
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ("user_name", "first_name", "last_name")
+
+class CarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Car
+        fields = ("vin", "year", "make", "model", "color")
