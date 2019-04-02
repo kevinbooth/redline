7a8508a7fd007d2e4bafafb2a13948e15cb4362f
atoscano199 <ajt1042@wildcats.unh.edu>
2019-04-02 09:24:20 -0400

    Removed the user class in models.py as we will be using the built in user model

--- redline/models.py
+++ redline/models.py
@@ -1,21 +1,11 @@
 """Stores all of the models that are needed for the API"""
 from django.db import models
 
-class User(models.Model):
-    name = models.CharField(
-            max_length=255, blank=False, unique=True)
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_modified = models.DateTimeField(auto_now=True)
-
-    def_str_(self):
-        return f'{self.name}'
-
 class Car(models.Model):
     name = models.CharField(
             max_length=255, blank=False, unique=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
 
-    def_str_(self):
+    def str(self):
         return f'{self.name}'
-        
