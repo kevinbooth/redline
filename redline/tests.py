afb3215e7f92d84ace2cd47be996ba4618d56b20
atoscano199 <ajt1042@wildcats.unh.edu>
2019-04-02 09:26:53 -0400

    Added an import for the built in user model in Django in tests.py.

--- redline/tests.py
+++ redline/tests.py
@@ -5,8 +5,9 @@ from django.test import TestCase
 from django.urls import reverse
 from rest_framework.test import APITestCase, APIClient
 from rest_framework.views import status
-from .models import User, Car
+from .models import Car
 from .serializers import UserSerializer, CarSerializer
+from django.contrib.auth.models import User
 
 
 class BaseViewTest(APITestCase):
