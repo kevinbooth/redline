a9eeaab9df8895cbdf643f2ba2041f86442f77d1
Kevin Booth <kdbooth97@gmail.com>
2019-04-03 15:15:35 -0400

    Add home view and connect to the home.html template

--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+#import requests
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+APP_TEMPLATE_DIR = ''
+API_ROOT_URL = 'http://localhost:8000/api/v1/'
+
+
+class HomeView(TemplateView):
+
+    template_name = APP_TEMPLATE_DIR + "home.html"
+
+    def get_context_data(self, **kwargs):
+        """
+        Override the get_context_data method to add new data to the
+        context dictionary that is passed to the template
+        """
+        context = super().get_context_data(**kwargs)
+        # context['greeting'] = 'Welcome to the Front End!'
+        # send a request to the api to retrieve the list of pilots
+        # car_list = self.get_from_api('cars')
+        # add pilot_list key to the context dictionary so we can use it
+        # in the template
+        # context['car_list'] = car_list
+        return context
+
+    def get_from_api(self, url):
+        """
+        Sends a get requests to API_ROOT_URL/url
+        @param url : string
+        @return json api response
+        """
+        # response = requests.get(API_ROOT_URL + url)
+        # data = response.json()
+        # return data
+        return
