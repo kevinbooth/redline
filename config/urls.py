3fc51944aff7309425072944b075d1768102e2ab
Kevin Booth <kdbooth97@gmail.com>
2019-04-03 15:14:53 -0400

    Connect frontend urls to project

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 
 urlpatterns = [
+    path('', include('frontend.urls')),
     path('admin/', admin.site.urls),
     re_path('api/(?P<version>(v1|v2))/', include('redline.urls')),
 ]
