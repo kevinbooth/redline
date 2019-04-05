a13c756e17f7edbc9b955de204b4936090dad7be
Kevin Booth <kdbooth97@gmail.com>
2019-04-04 23:44:15 -0400

    Connect url to car template

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,11 +3,13 @@ from .views import HomeView
 from .views import LoginView
 from .views import RegisterView
 from .views import NewCarView
+from .views import CarView
 
 app_name = 'frontend'
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('login', LoginView.as_view(), name="login"),
     path('register', RegisterView.as_view(), name="register"),
-    path('new-car', NewCarView.as_view(), name="new-car")
+    path('new-car', NewCarView.as_view(), name="new-car"),
+    path('car/<id>', CarView.as_view(), name="car")
 ]
