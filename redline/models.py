79f6b6fff334c3d6b771459363a4c6802151770d
atoscano199 <ajt1042@wildcats.unh.edu>
2019-04-09 19:37:36 -0400

    Created the Task model class in models.py

--- redline/models.py
+++ redline/models.py
@@ -1,2 +1,12 @@
 """Stores all of the models that are needed for the API"""
+import uuid
+
 from django.db import models
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=255, null=False)
+    estimated_hours = models.IntegerField(default=0)
+    due_date = models.
+    completion_date = models.
+    notes = models.
