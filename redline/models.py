70a7c4d5bc354e86b09fa67e13df1cdc3bc3b9c5
atoscano199 <ajt1042@wildcats.unh.edu>
2019-04-09 19:48:15 -0400

    Finalized Task model class

--- redline/models.py
+++ redline/models.py
@@ -7,6 +7,9 @@ from django.db import models
 class Task(models.Model):
     name = models.CharField(max_length=255, null=False)
     estimated_hours = models.IntegerField(default=0)
-    due_date = models.
-    completion_date = models.
-    notes = models.
+    due_date = models.DateField(null=False, blank=False)
+    completion_date = models.DateField(auto_now_add=True)
+    notes = models.CharField(max_length=500, null=False)
+
+    def __str__(self):
+        return "{}, {}".format(self.name, self.estimated_hours, self.due_date, self.due_date, self.completion_date, self.notes)
