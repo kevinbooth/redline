741f1fdfa2cab2e535e847cde0ade78001a6200b
Kevin Booth <kdbooth97@gmail.com>
2019-04-13 22:45:39 -0400

    Direct logout url to logout view'

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
 from .views import HomeView
 from .views import LoginView
 from .views import RegisterView
@@ -9,9 +10,9 @@ from .views import CarView
 app_name = 'frontend'
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
-    path('login', LoginView.as_view(), name="login"),
+    path('login', auth_views.LoginView.as_view(template_name='frontend/login.html'), name="login"),
+    path('logout', LogoutView.as_view(), name="logout"),
     path('register', RegisterView.as_view(), name="register"),
     path('new-car', NewCarView.as_view(), name="new-car"),
     path('car/<id>', CarView.as_view(), name="car"),
-    path('login2', auth_views.LoginView.as_view(template_name='frontend/login.html'), name="login2")
 ]
