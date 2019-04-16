d4dfa11b17b9dbebe1204f8af71f70c8c03c832f
atoscano199 <ajt1042@wildcats.unh.edu>
2019-04-16 10:52:47 -0400

    Added the car_id and updated completion date

--- redline/models.py
+++ redline/models.py
@@ -21,8 +21,8 @@ class Task(models.Model):
     name = models.CharField(max_length=255, null=False)
     estimated_hours = models.IntegerField(default=0)
     due_date = models.DateField(null=False, blank=False)
-    completion_date = models.DateField(null=False, blank=False)
-    notes = models.CharField(max_length=500, null=False)
+    completion_date = models.DateField(null=True, blank=True)
+    notes = models.CharField(max_length=500, null=False, blank=True)
 
     def __str__(self):
         return "{}".format(self.name)
