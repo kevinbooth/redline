0b2186e47370a4c6f6a36006fab986e0cab3a251
atoscano199 <ajt1042@wildcats.unh.edu>
2019-04-15 23:06:23 -0400

    Added missing import to views/py

--- redline/views.py
+++ redline/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Car, Task
 from .serializers import CarSerializer, TaskSerializer
+from .serializers import CarPostSerializer, TaskPostSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
