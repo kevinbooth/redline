57a911a8ed8a88fc60e41858a56b3bab117ae1c7
atoscano199 <ajt1042@wildcats.unh.edu>
2019-04-15 23:15:00 -0400

    Added car views to views.py from github

--- redline/views.py
+++ redline/views.py
@@ -7,6 +7,59 @@ from rest_framework.response import Response
 from rest_framework import status
 
 
+class CarView(APIView):
+    """
+    This class handles the GET and POST actions for the Car resource.
+    GET - Retrieves a list of all Cars
+    POST - Creates a new car
+    """
+    def get(self, request, version, format=None):
+        cars = Car.objects.all()
+        serializer = CarSerializer(cars, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, version, format=None):
+        serializer = CarPostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CarObjectView(APIView):
+    """
+    This class handles GET, PUT, and DELETE actions for the Car resource.
+    GET - Retrieves a single car
+    PUT - Updates a single cars information
+    DELETE - Removes a car from the list
+    """
+    def get_object(self, id):
+        try:
+            return Car.objects.get(id=id)
+        except Car.DoesNotExist:
+            return None
+
+    def get(self, request, version, id, format=None):
+        car = self.get_object(id)
+        serializer = CarSerializer(car)
+        return Response(serializer.data)
+
+    def put(self, request, version, id, format=None):
+        car = self.get_object(id)
+        serializer = CarSerializer(car, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, version, id, format=None):
+        car = self.get_object(id)
+        if car:
+            car.delete()
+            return Response(status.HTTP_200_OK)
+        return Response(status.HTTP_404_NOT_FOUND)
+
+
 class View(APIView):
     """
     This class handles the GET and POST actions for the Task resource.
