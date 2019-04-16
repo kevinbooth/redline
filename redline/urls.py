88721fd155e4517953d8d5f9ab7b11847482bf5c
atoscano199 <ajt1042@wildcats.unh.edu>
2019-04-16 10:53:27 -0400

    Updated urls for Taskview

--- redline/urls.py
+++ redline/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('user/', include('account.urls')),
     path('cars/', CarView.as_view(), name="cars"),
     path('car/<id>', CarObjectView.as_view(), name="car"),
-    path('tasks/', TaskView.as_view(), name="tasks"),
+    path('car/<id>/tasks/', TaskView.as_view(), name="tasks"),
     path('task/<id>', TaskObjectView.as_view(), name="task")
 ]
