cb4a7f10237c9dfb42cb56d658acae6ac9ae2c85
Kevin Booth <kdbooth97@gmail.com>
2019-04-17 11:50:39 -0400

    Update init file imports

---
+++ redline/tests/__init__.py
@@ -0,0 +1,9 @@
+import car_endpoint_tests
+import task_endpoint_tests
+import part_endpoint_tests
+
+__all__ = [
+    'car_endpoint_tests',
+    'task_endpoint_tests',
+    'part_endpoint_tests'
+]
---
+++ redline/views/__init__.py
@@ -0,0 +1,15 @@
+from .car_object_view import CarObjectView
+from .car_view import CarView
+from .part_object_view import PartObjectView
+from .part_view import PartView
+from .task_object_view import TaskObjectView
+from .task_view import TaskView
+
+__all__ = [
+    'CarObjectView',
+    'CarView',
+    'PartObjectView',
+    'PartView',
+    'TaskObjectView',
+    'TaskView'
+]
