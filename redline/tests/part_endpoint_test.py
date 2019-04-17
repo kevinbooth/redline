ee95433b8d60f0ce24e1f9aa57eed26db4a8b0eb
Kevin Booth <kdbooth97@gmail.com>
2019-04-17 11:31:22 -0400

    Add part_endpoint_test module in redline/tests package

---
+++ redline/tests/part_endpoint_test.py
@@ -0,0 +1,23 @@
+from rest_framework.test import APITestCase, APIClient
+from rest_framework.views import status
+from django.urls import reverse
+# from .models import Part
+# from .serializers import PartSerializer, PartPostSerializer
+
+
+
+class BaseViewTest(APITestCase):
+    client = APIClient()
+
+    def setUp(self):
+        ()
+
+
+class PartEndpointTest(BaseViewTest):
+    def test_put_action(self):
+
+        self.assertEquals(True, False)
+
+    def test_delete_action(self):
+
+        self.assertEquals(True, False)
