7f287a6dec58f248e4ffadc032a4301fb5a3e58b
Kevin Booth <kdbooth97@gmail.com>
2019-04-17 12:53:34 -0400

    Update test package init files

--- account/tests/__init__.py
+++ account/tests/__init__.py
@@ -1,5 +1,6 @@
 from .user_endpoint_test import BaseViewTest as UserBase, UserEndpointTest
 
 __all__ = [
+    'UserBase'
     'UserEndpointTest'
 ]
--- redline/tests/__init__.py
+++ redline/tests/__init__.py
@@ -3,7 +3,10 @@ from .task_endpoint_tests import BaseViewTest as TaskBase, TaskEndpointTest
 from .part_endpoint_tests import BaseViewTest as PartBase, PartEndpointTest
 
 __all__ = [
+    'CarBase',
     'CarEndpointTest',
+    'TaskBase',
     'TaskEndpointTest',
+    'PartBase',
     'PartEndpointTest'
 ]
