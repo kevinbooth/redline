402a37cb6720203ff8c3d76363cfa58771d84779
Kevin Booth <kdbooth97@gmail.com>
2019-04-17 08:41:35 -0400

    Add car model module

---
+++ redline/models/car_model.py
@@ -0,0 +1,17 @@
+"""
+Module for the Car model definition
+"""
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Car(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    vin = models.CharField(max_length=17, null=False)
+    year = models.IntegerField(null=False)
+    make = models.CharField(max_length=255, null=False)
+    model = models.CharField(max_length=255, null=False)
+    color = models.CharField(max_length=255, null=False)
+
+    def __str__(self):
+        return "{}, {}".format(self.make, self.model)
