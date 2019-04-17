f8a33dc4c040e2b4c15e9cfa0af8628cbb7973e4
Kevin Booth <kdbooth97@gmail.com>
2019-04-17 11:52:12 -0400

    Add part endpoint views to redline/views package

---
+++ redline/views/part_object_view.py
@@ -0,0 +1,39 @@
+from redline.models import Part
+from redline.serializers import PartSerializer, PartPostSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+
+class PartObjectView(APIView):
+    """
+    This class handles GET, PUT, and Delete actions for the Part resource.
+    GET - Retrieves a single part
+    PUT - Updates a single parts information
+    Delete - Removes a part from the list
+    """
+    def get_object(self, id):
+        try:
+            return Part.objects.get(id=id)
+        except Part.DoesNotExist:
+            return None
+
+    def get(self, request, version, id, format=None):
+        part = self.get_object(id)
+        serializer = PartSerializer(part)
+        return Response(serializer.data)
+
+    def put(self, request, version, id, format=None):
+        part = self.get_object(id)
+        serializer = artSerializer(part, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, version, id, format=None):
+        part = self.get_object(id)
+        if part:
+            part.delete()
+            return Response(status.HTTP_200_OK)
+        return Response(status.HTTP_404_NOT_FOUND)
---
+++ redline/views/part_view.py
@@ -0,0 +1,24 @@
+from redline.models import Part
+from redline.serializers import PartSerializer, PartPostSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+
+class PartView(APIView):
+    """
+    This class handles the GET and POST actions for the Part resource.
+    GET - Retrieves a list of all Parts
+    POST - Creates a new Part
+    """
+    def get(self, request, version, format=None):
+        parts = Part.objects.all()
+        serializer = PartSerializer(parts, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, version, format=None):
+        serializer = PartPostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
