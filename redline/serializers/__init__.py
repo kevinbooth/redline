f2447c53cd5c390408f4d4852e14842d44e77b65
Kevin Booth <kdbooth97@gmail.com>
2019-04-17 08:51:23 -0400

    Update imports in serializers/__init__.py

---
+++ redline/serializers/__init__.py
@@ -0,0 +1,15 @@
+from .car_post_serializer import CarPostSerializer
+from .car_serializer import CarSerializer
+from .part_post_serializer import PartPostSerializer
+from .part_serializer import PartSerializer
+from .task_post_serializer import TaskPostSerializer
+from .task_serializer import TaskSerializer
+
+__all__ = [
+    'CarPostSerializer',
+    'CarSerializer',
+    'PartPostSerializer',
+    'PartSerializer',
+    'TaskPostSerializer',
+    'TaskSerializer'
+]
