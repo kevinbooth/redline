08f506be421c250d319a6ddf3456e3953bfc3d54
Kevin Booth <kdbooth97@gmail.com>
2019-04-17 08:48:35 -0400

    Add task serializer modules to serializer package

---
+++ redline/serializers/task_serializer.py
@@ -0,0 +1,11 @@
+"""
+Used to convert data to Python data types then rendered to JSON
+"""
+from rest_framework import serializers
+from .models import Task
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ("name", "estimated_hours", "due_date", "completion_date", "notes")
