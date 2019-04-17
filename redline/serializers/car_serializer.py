b0f4b11b35e8b81c80c4ea095cdcc0615bfcb3b4
Kevin Booth <kdbooth97@gmail.com>
2019-04-17 11:50:08 -0400

    Update serializer imports to be absolute rather than relative

--- redline/serializers/car_serializer.py
+++ redline/serializers/car_serializer.py
@@ -2,7 +2,7 @@
 Used to convert data to Python data types then rendered to JSON
 """
 from rest_framework import serializers
-from .models import Car
+from redline.models import Car
 
 
 class CarSerializer(serializers.ModelSerializer):
--- redline/serializers/part_serializer.py
+++ redline/serializers/part_serializer.py
@@ -2,7 +2,7 @@
 Used to convert data to Python data types then rendered to JSON
 """
 from rest_framework import serializers
-from .models import Part
+from redline.models import Part
 
 
 class PartSerializer(serializers.ModelSerializer):
