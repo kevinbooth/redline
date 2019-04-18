214b59f1c041c1045832498584423b4c343b9d6c
Kevin Booth <kdbooth97@gmail.com>
2019-04-18 17:29:24 -0400

    Add logic to send request to backend API for dashboard view

--- frontend/views/home_view.py
+++ frontend/views/home_view.py
@@ -1,5 +1,5 @@
 from frontend import views
-# import requests
+import requests
 from django.views.generic.base import TemplateView
 
 APP_TEMPLATE_DIR = 'frontend/'
@@ -16,21 +16,20 @@ class HomeView(TemplateView):
         context dictionary that is passed to the template
         """
         context = super().get_context_data(**kwargs)
-        # context['greeting'] = 'Welcome to the Front End!'
-        # send a request to the api to retrieve the list of pilots
-        # car_list = self.get_from_api('cars')
-        # add pilot_list key to the context dictionary so we can use it
-        # in the template
-        # context['car_list'] = car_list
+        car_list = self.get_from_api('cars/', self.request.user.auth_token)
+        context['car_list'] = car_list
+
         return context
 
-    def get_from_api(self, url):
+    def get_from_api(self, url, auth):
         """
         Sends a get requests to API_ROOT_URL/url
         @param url : string
         @return json api response
         """
-        # response = requests.get(API_ROOT_URL + url)
-        # data = response.json()
-        # return data
-        return
+        response = requests.get(
+                                API_ROOT_URL + url,
+                                headers={'Authorization': 'Token ' + str(auth)}
+                                )
+        data = response.json()
+        return data
