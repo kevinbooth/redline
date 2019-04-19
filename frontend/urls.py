dba3c62fbb6706c336c718152908623fdbe35790
Kevin Booth <kdbooth97@gmail.com>
2019-04-18 22:42:21 -0400

    Update frontend urls to have forward slashes at the end

--- frontend/urls.py
+++ frontend/urls.py
@@ -8,15 +8,16 @@ from .views import NewCarView
 from .views import CarView
 
 app_name = 'frontend'
+
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path(
-            'login',
+            'login/',
             auth_views.LoginView.as_view(template_name='frontend/login.html'),
             name="login"
         ),
-    path('logout', LogoutView.as_view(), name="logout"),
-    path('register', RegisterView.as_view(), name="register"),
-    path('new-car', NewCarView.as_view(), name="new-car"),
-    path('car/<id>', CarView.as_view(), name="car"),
+    path('logout/', LogoutView.as_view(), name="logout"),
+    path('register/', RegisterView.as_view(), name="register"),
+    path('new-car/', NewCarView.as_view(), name="new-car"),
+    path('car/<id>/', CarView.as_view(), name="car"),
 ]
