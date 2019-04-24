23e3d73ee73b2b091eac5ae106a6730ec072e311
Kevin Booth <kdbooth97@gmail.com>
2019-04-23 21:59:35 -0400

    Add new_part_view module and class to frontend

--- frontend/views/__init__.py
+++ frontend/views/__init__.py
@@ -5,6 +5,7 @@ from .login_view import LoginView
 from .register_view import RegisterView
 from .new_car_view import NewCarView
 from .new_task_view import NewTaskView
+from .new_part_view import NewPartView
 
 __all__ = [
     'CarView',
@@ -14,4 +15,5 @@ __all__ = [
     'RegisterView'
     'NewCarView',
     'NewTaskView',
+    'NewPartView'
 ]
---
+++ frontend/views/new_part_view.py
@@ -0,0 +1,75 @@
+from frontend.constants import APP_TEMPLATE_DIR, API_ROOT_URL
+import requests
+from django.views.generic.base import TemplateView
+from django.shortcuts import render
+from frontend.forms import NewPartForm
+
+
+class NewPartView(TemplateView):
+
+    template_name = APP_TEMPLATE_DIR + "new-part.html"
+
+    def get_context_data(self, car_id, task_id, **kwargs):
+        """
+        Override the get_context_data method to add new data to the
+        context dictionary that is passed to the template
+        """
+        context = super().get_context_data(**kwargs)
+        car = self.get_from_api('car/' + car_id,
+                                self.request.user.auth_token
+                                )
+        task = self.get_from_api('task/' + task_id,
+                                self.request.user.auth_token
+                                )
+        context['car'] = car
+        context['task'] = task
+
+        return context
+
+    def post(self, request, car_id, task_id, **kwargs):
+        context = self.get_context_data(car_id, task_id)
+        form = NewPartForm(self.request.POST)
+
+        if form.is_valid():
+            form.cleaned_data['task_id'] = task_id
+            self.post_to_api(
+                             'parts/',
+                             self.request.user.auth_token,
+                             form.cleaned_data
+                             )
+            context['message'] = 'Thank you! Your part has been saved.'
+            return render(request, self.template_name, context)
+        else:
+            context['message'] = 'There was an error with your request.'
+            return render(request, self.template_name, context)
+
+    def get_from_api(self, url, auth):
+        """
+        Sends a get requests to API_ROOT_URL/url
+        @param url : string
+        @return json api response
+        """
+        response = requests.get(
+                                API_ROOT_URL + url,
+                                headers={'Authorization': 'Token ' + str(auth)}
+                                )
+        data = response.json()
+        return data
+
+    def post_to_api(self, url, auth, data=''):
+        """
+        Sends a get requests to API_ROOT_URL/url
+        @param url : string
+        @param data : json (optional)
+        @return json api response
+        """
+        response = requests.post(
+                                 API_ROOT_URL + url,
+                                 headers={
+                                          'Authorization': 'Token ' +
+                                          str(auth)
+                                          },
+                                 json=data
+                                 )
+        data = response.json()
+        return data
