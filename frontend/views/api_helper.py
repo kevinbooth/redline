b29c288a02d9b71fd6a0d5646fbb4d6ea20eb3ed
Kevin Booth <kdbooth97@gmail.com>
2019-04-24 14:47:30 -0400

    Condense repeated functions into new api_helper module

---
+++ frontend/views/api_helper.py
@@ -0,0 +1,57 @@
+from frontend.constants import APP_TEMPLATE_DIR, API_ROOT_URL
+import requests
+
+
+class APIHelper():
+    def get_from_api(url, auth):
+        """
+        Sends a get requests to API_ROOT_URL/url
+        @param url : string
+        @return json api response
+        """
+        response = requests.get(API_ROOT_URL + url,
+                                headers={'Authorization': 'Token ' + str(auth)}
+                                )
+        print(response)
+        data = response.json()
+        return data
+
+    def post_to_api(url, auth, data=''):
+        """
+        Sends a get requests to API_ROOT_URL/url
+        @param url : string
+        @param data : json (optional)
+        @return json api response
+        """
+        response = requests.post(API_ROOT_URL + url,
+                                 headers={'Authorization': 'Token ' +
+                                          str(auth)},
+                                 json=data)
+
+        data = response.json()
+        return data
+
+    def public_post_to_api(url, data=''):
+        """
+        Sends a get requests to API_ROOT_URL/url
+        @param url : string
+        @param data : json (optional)
+        @return json api response
+        """
+        response = requests.post(API_ROOT_URL + url, json=data)
+        data = response.json()
+        return data
+
+    def put_to_api(url, auth, data=''):
+        """
+        Sends a get requests to API_ROOT_URL/url
+        @param url : string
+        @param data : json (optional)
+        @return json api response
+        """
+        response = requests.put(API_ROOT_URL + url,
+                                headers={'Authorization': 'Token ' +
+                                         str(auth)},
+                                json=data)
+        data = response.json()
+        return data
