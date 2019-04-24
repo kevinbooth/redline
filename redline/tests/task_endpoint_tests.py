5d9487e86088e9ffef168c7863a1630635157137
atoscano199 <ajt1042@wildcats.unh.edu>
2019-04-23 20:05:12 -0400

    Added imports and setup for task_endpoint_tests

--- redline/tests/task_endpoint_tests.py
+++ redline/tests/task_endpoint_tests.py
@@ -1,15 +1,54 @@
+
+"""
+Module to test task endpoints in the redline app
+"""
 from rest_framework.test import APITestCase, APIClient
+from rest_framework.authtoken.models import Token
 from rest_framework.views import status
 from django.urls import reverse
-# from .models import Task
-# from .serializers import TaskSerializer, TaskPostSerializer
+from django.contrib.auth.models import User
+from redline.models import Car
 
 
 class BaseViewTest(APITestCase):
     client = APIClient()
+    car_post_data = {}
+    user_id = 0
 
     def setUp(self):
-        ()
+        User.objects.create_user(username="jsmith",
+                                 email="jsmith@unh.edu",
+                                 password="abc123",
+                                 first_name="John",
+                                 last_name="Smith")
+        user_post_data = {
+            'username': 'jsmith',
+            'password': 'abc123'
+        }
+        response = self.client.post(
+            reverse("auth", kwargs={'version': 'v1'}),
+            user_post_data,
+            format='json'
+        )
+
+        self.user_id = User.objects.get(username='jsmith').id
+        self.car_post_data = {
+            'user_id': self.user_id,
+            'vin': 'abc1234567890',
+            'year': '2013',
+            'make': 'Toyota',
+            'model': 'Corolla',
+            'color': 'White'
+        }
+        token = Token.objects.get(user__username='jsmith')
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+
+        response = self.client.post(
+            reverse("cars", kwargs={'version': 'v1'}),
+            self.car_post_data,
+            format='json'
+        )
+
 
 
 class TaskEndpointTest(BaseViewTest):
