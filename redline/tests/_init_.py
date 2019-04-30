210cd6642e297f4ab2ac39bf9100e14b560f5e14
atoscano199 <ajt1042@wildcats.unh.edu>
2019-04-29 22:22:21 -0400

    Made several changes to the unit tests, ready for review

--- redline/tests/_init_.py
+++ redline/tests/_init_.py
@@ -1,6 +1,6 @@
 from .car_endpoint_tests import CarEndpointTest
-from .task_endpoint_tests import BaseViewTest as TaskBase, TaskEndpointTest
-from .part_endpoint_tests import BaseViewTest as PartBase, PartEndpointTest
+from .task_endpoint_tests import TaskEndpointTest
+from .part_endpoint_tests import PartEndpointTest
 
 __all__ = [
     'CarBase',
